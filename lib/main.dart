bf4dcf82010f833b7b15e0e6894017e8f86dce1d
Mondanai <6631501093@lamduan.mfu.ac.th>
2025-09-05 10:18:45 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_week5/week04/basic.dart';
+
+
+void main() {
+runApp(MaterialApp(home: Basic()));
+}
+   
+
+
---
+++ lib/week04/basic.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+class Basic extends StatelessWidget {
+  const Basic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+Scaffold(
+          appBar: AppBar(
+            title: const Text("My first app" , style: TextStyle(color: Colors.white),),
+            backgroundColor:  Color.fromARGB(255, 255, 0, 255),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text("Hello world",
+            style: TextStyle(fontSize: 22, color: Colors.pink),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: null,
+            child: const Icon(Icons.add,color: Color.fromARGB(255, 255, 255, 255),),
+            backgroundColor: Colors.red[600],   ), 
+);
+  }
+}
