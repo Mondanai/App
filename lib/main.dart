fd6ed7a9fa0ca4628ab29401db96e1ee4d795747
Mondanai <6631501093@lamduan.mfu.ac.th>
2025-09-19 10:38:21 +0700

    baking

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_week5/week6/images.dart';
+import 'package:flutter_application_week5/baking_app.dart';
 
 
 void main() {
   //basic
-runApp(MaterialApp(home:ImagesDemo()));
+runApp(MaterialApp(home:BakingApp()));
 }
    
 
