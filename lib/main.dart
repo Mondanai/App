7ab8841e136ae72358728dbaa693df1f5b42f14d
Mondanai <6631501093@lamduan.mfu.ac.th>
2025-10-10 10:28:28 +0700

    alertdialogdemo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_week5/week7/timepickerdemo.dart';
+import 'package:flutter_application_week5/week7/alertdialogdemo.dart';
+
 
 
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TimePickerDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: AlertDialogDemo()));
 }
---
+++ lib/week7/alertdialogdemo.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+
+class AlertDialogDemo extends StatefulWidget {
+  const AlertDialogDemo({super.key});
+
+
+  @override
+  State<AlertDialogDemo> createState() => _AlertDialogDemoState();
+}
+
+
+class _AlertDialogDemoState extends State<AlertDialogDemo> {
+  void showAlert() async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Alert title'),
+          content: Text('Alert content'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Navigator.pop(context);
+              },
+              child: Text('OK'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alert Dialog'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: FilledButton(
+          onPressed: showAlert,
+          child: const Text('alert'),
+        ),
+      ),
+    );
+  }
+}
