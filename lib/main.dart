5107869503249c430d791dc09944ac65830dbd7a
Mondanai <6631501093@lamduan.mfu.ac.th>
2025-10-10 09:35:23 +0700

    sliderdemo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_week5/week7/checkboxdemo.dart';
+import 'package:flutter_application_week5/week7/sliderdemo.dart';
+
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: CheckboxDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SliderDemo()));
 }
