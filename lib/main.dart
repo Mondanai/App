f9dc9bc333723341afc0030403ed4f3d4af5dc8c
Mondanai <6631501093@lamduan.mfu.ac.th>
2025-10-10 10:18:01 +0700

    datepickerdemo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_week5/week7/dropdowndemo.dart';
+import 'package:flutter_application_week5/week7/datepickerdemo.dart';
 
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: DropdownDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: DatePickerDemo()));
 }
