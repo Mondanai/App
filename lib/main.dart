1079715106878b2e62139af164b1fe31e88da7c6
Mondanai <6631501093@lamduan.mfu.ac.th>
2025-10-10 10:20:06 +0700

    timepickerdemo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_week5/week7/datepickerdemo.dart';
+import 'package:flutter_application_week5/week7/timepickerdemo.dart';
+
 
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: DatePickerDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TimePickerDemo()));
 }
