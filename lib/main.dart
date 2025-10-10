9cb9acffd4101b9f0551c1e065a1fb8fa8231e5b
Mondanai <6631501093@lamduan.mfu.ac.th>
2025-10-10 09:42:03 +0700

    radiodemo ‘value’ and ‘groupValue’

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_week5/week7/sliderdemo.dart';
+import 'package:flutter_application_week5/week7/radiodemo.dart';
+
 
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SliderDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: RadioDemo()));
 }
